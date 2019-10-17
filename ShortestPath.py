a8945dd6983322695f12202efe7599a60c9192d9
truongcntn2017 <1712195@student.hcmus.edu.vn>
2019-10-17 23:13:36 +0700

    Modified ShortestPath.py

--- ShortestPath.py
+++ ShortestPath.py
@@ -171,7 +171,7 @@ class ShortestPath :
       return -1
     return self.backtrace(parent, start, goal)
 
-  def BFS(self,start,end) :
+  def breadth_first_search(self,start,end) :
     xa,ya = start
     xb,yb = end
     
@@ -193,6 +193,32 @@ class ShortestPath :
         queue.append([next,cost+1])
         parent[next] = (current)
     return -1
+
+  def depth_first_search(matrix, x_start,y_start,x_end,y_end):
+	  path=[]
+	  queue=[(x_start,y_start)]
+
+	  while (len(queue)!=0):
+		  current=queue.pop(0)
+		  path.append(current)
+		  x,y=current
+		  if x == x_end and y == y_end:
+			  break
+		  matrix[y][x]=1
+
+		  if(matrix[y][x-1]!=0 and matrix[y-1][x]!=0 and matrix[y+1][x]!=0 and matrix[y][x+1]!=0):
+			  path.pop(len(path)-1)
+		  if(matrix[y][x-1]==0):
+			  queue.insert(0,(x-1,y))
+		  if(matrix[y-1][x]==0):
+			  queue.insert(0,(x,y-1))
+		  if(matrix[y][x+1]==0):
+			  queue.insert(0,(x+1,y))
+		  if(matrix[y+1][x]==0):
+			  queue.insert(0,(x,y+1))
+        
+	  return path
+
 
 #######################################
 width,height, start, end, polygons = read_file("/home/truongtop14/git_workspace/AI-Searching-Algorithm-Project/TestCase/input_0.txt")
